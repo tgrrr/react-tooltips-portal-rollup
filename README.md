b4b88cbf1b8ea940388d0a57436905ac51c7149b
Phil Steinke <phil@tgrrr.com>
2020-12-30 17:25:08 +0800

    feature: package.json update packages to @rollup and keywords
    feature: babel config file with rollup

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # Tooltips
 
-
 Build package:
 
 ```zsh
 $ yarn build
 
+> yarn run v1.22.10
 > $ rollup -c
 
 > src/index.js → dist/bundle.js...
-> (!) Generated an empty chunk
-> index
-> created dist/bundle.js in 11ms
-> ✨  Done in 0.73s.
+> created dist/bundle.js in 556ms
+> ✨  Done in 1.04s.
 ```
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
-export default {
+import babel from '@rollup/plugin-babel';
+
+const config = {
 	input: 'src/index.js',
 	output: {
-	  file: 'dist/bundle.js',
-	  format: 'cjs'
-	}
-  };
-  
+		file: 'dist/bundle.js',
+		exports: 'named',
+		format: 'cjs'
+	},
+	external: [
+		'react',
+		],
+	plugins: [
+		babel({ 
+			babelHelpers: 'bundled',
+			exclude: 'node_modules/**'
+		}),
+	]
+};
+
+export default config;
